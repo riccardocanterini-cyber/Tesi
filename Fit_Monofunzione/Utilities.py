caf5b93b4d659682896c93d2f7fae1a1640645a4
riccardocanterini-cyber <riccardocanterini@gmail.com>
2026-06-24 13:28:54 +0200

    Prova Fit Bifunzione (BF-BifunctionFit)

---
+++ Fit_Monofunzione/Utilities.py
@@ -0,0 +1,22 @@
+import ROOT
+import uproot 
+import awkward as ak
+from scipy.stats import crystalball
+
+def extractor(file_path, tree_name):
+    file=uproot.open(file_path)
+    tree=file[tree_name]
+    branches=["FatJet_particleNet_mass", "FatJet_particleNetMD_Xbb", "FatJet_particleNetMD_QCD"]
+    events=tree.arrays(branches, library="ak")
+    Xbb=events["FatJet_particleNetMD_Xbb"]
+    QCD=events["FatJet_particleNetMD_QCD"]      
+    #filter=FatJet_particleNetMD_Xbb/(FatJet_particleNetMD_QCD+FatJet_particleNetMD_Xbb) >= 0.98
+    #Questo è il filtro datoci da Carlo (Chiedere al prof cos'è QCD)
+    filtro_25=Xbb/(QCD+Xbb) >= 0.98
+    dati_FatJet=tree.arrays("FatJet_particleNet_mass", library="ak", cut=filtro_25)
+    #Otteniamo un array piatto col quale possiamo lavorare
+    dati_piatti=ak.flatten(dati_FatJet["FatJet_particleNet_mass"])    
+    return dati_piatti
+
+def cb_pdf(x, mu, sigma, beta, m):
+    return crystalball.pdf(-1*x, beta, m, loc=-mu, scale=sigma  )
